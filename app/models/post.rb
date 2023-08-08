39d88668347a5f0da8dee3ae70d6dab3b79732e9
imhanzala22 <hanzala1803@gmail.com>
2023-08-08 10:37:18 +0500

    qr scane test

---
+++ app/models/post.rb
@@ -0,0 +1,33 @@
+class Post < ApplicationRecord
+  include Rails.application.routes.url_helpers
+  has_one_attached :qrcode, dependent: :destroy
+  before_commit :generate_qrcode, on: :create
+
+  private
+
+  def generate_qrcode
+    host = Rails.application.config.action_controller.default_url_options[:host] # Correct way to get the host
+    qrcode = RQRCode::QRCode.new(post_url(self, host: host))
+     # Correct the parameters for post_url
+
+    png = qrcode.as_png(
+      bit_depth: 1,
+      border_modules: 4,
+      color_mode: ChunkyPNG::COLOR_GRAYSCALE, # Correct the constant name
+      color: "black",
+      file: nil,
+      fill: "white",
+      module_px_size: 6,
+      resize_exactly_to: false,
+      resize_gte_to: false,
+      size: 120
+    )
+
+    self.qrcode.attach(
+      io: StringIO.new(png.to_s),
+      filename: "qrcode.png",
+      content_type: "image/png"
+    )
+  end
+
+end
